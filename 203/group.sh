e5d1bf0122956e5f017e04e9013d1ab4e22a321f
whilliker <whilliker@monroeccc.edu>
2023-12-10 12:15:34 -0500

    Update group.sh
    add search for group members

--- 203/group.sh
+++ 203/group.sh
@@ -7,5 +7,10 @@ groups
 
 members adm
 
+echo -e "\e[1;32m Show adm Group Memember \e[0m"
 grep '^adm:.*$' /etc/group | cut -d: -f4
-# syslog,abhishek
+
+echo -e "\e[1;32m Show sudo Group Memember \e[0m"
+grep '^sudo:.*$' /etc/group | cut -d: -f4
+
+
